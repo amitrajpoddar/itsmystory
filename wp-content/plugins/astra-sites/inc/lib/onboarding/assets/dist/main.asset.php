88547ec5737411c01939b0c173c4eb797dc69f77
Amit Raj <53418736+amitraj360@users.noreply.github.com>
2022-02-17 02:57:48 +0530

    3rd

--- wp-content/plugins/astra-sites/inc/lib/onboarding/assets/dist/main.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-media-utils', 'wp-polyfill', 'wp-url'), 'version' => '2c1ca3d0ef0dea21c363ce34f1e193fb');
